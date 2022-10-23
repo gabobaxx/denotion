b357d88d8e3ae9e9791e12aad3726ec9c44c66c6
gabrielba15 <gabriely101@hotmail.com>
2022-10-23 09:39:13 -0400

    html render and google verification tag

--- src/drive.google.ts
+++ src/drive.google.ts
@@ -1,10 +1,20 @@
 import { v4 as uuid } from 'https://deno.land/std@0.82.0/uuid/mod.ts';
 
+const HTML = await Deno.readFile(
+	`${new URL('.', import.meta.url).pathname}index.html`
+);
+
 export const handler = (request: Request): Response => {
 	try {
 		const id = uuid.generate();
-		console.log({ id, request });
-		return new Response('Hello World', { status: 200 });
+		console.log({ request, id });
+		return new Response(HTML, {
+			status: 200,
+			headers: {
+				'content-type': 'text/html',
+			},
+		});
+		// return new Response(response, { status: 200 });
 	} catch (error) {
 		console.error(error);
 		const body = `Error: ${error.message}`;
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,5 @@ import { serve } from 'https://deno.land/std@0.160.0/http/server.ts';
 import { handler } from './drive.google.ts';
 
 const port = 8080;
-
 console.log(`Server running.`);
 await serve(handler, { port });
