7b163bab9ad452fda7648324e8141cf5b915564a
gabrielba15 <gabriely101@hotmail.com>
2022-10-24 08:59:34 -0400

    testing env vars

--- src/drive.google.ts
+++ src/drive.google.ts
@@ -4,17 +4,46 @@ const HTML = await Deno.readFile(
 	`${new URL('.', import.meta.url).pathname}index.html`
 );
 
-export const handler = (request: Request): Response => {
+const folderId = Deno.env.get('GOOGLE_BOOKS_FOLDER_ID');
+const token = Deno.env.get('GOOGLE_TOKEN');
+
+const singleFileUrl = (id: string | undefined) =>
+	`https://www.googleapis.com/drive/v3/files/${id}/watch`;
+
+async function generateAPIChannel(): Promise<Response> {
+	const id = uuid.generate();
+	const response = await fetch(singleFileUrl(folderId), {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${token}`,
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			id, // Your channel ID.
+			type: 'web_hook',
+			address: 'https://denotion.deno.dev/notification',
+		}),
+	});
+	console.log(response);
+	return new Response(response.body, {
+		status: 200,
+		headers: {
+			'content-type': 'text/html',
+		},
+	});
+}
+
+export const handler = async (host: string, request: Request) => {
 	try {
-		const id = uuid.generate();
-		console.log({ request, id });
+		if (request.url === `${host}/generate`) {
+			return await generateAPIChannel();
+		}
+		if (request.url === `${host}/notification`) {
+			return Response.redirect(`${host}`);
+		}
 		return new Response(HTML, {
 			status: 200,
-			headers: {
-				'content-type': 'text/html',
-			},
 		});
-		// return new Response(response, { status: 200 });
 	} catch (error) {
 		console.error(error);
 		const body = `Error: ${error.message}`;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { serve } from 'https://deno.land/std@0.160.0/http/server.ts';
 import { handler } from './drive.google.ts';
 
-const port = 8080;
+const PORT = 8080;
+const ENV = Deno.env.get('ENVIROMENT');
+const HOST =
+	ENV === 'production'
+		? 'https://denotion.deno.dev'
+		: `http://localhost:${PORT}`;
+
 console.log(`Server running.`);
-await serve(handler, { port });
+await serve(async (REQUEST: Request) => await handler(HOST, REQUEST), {
+	port: PORT,
+});
